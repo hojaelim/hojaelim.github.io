4934fb75d78485eb49843a05b13968bfabbffbb8
hojaelim <hobae1230@gmail.com>
2025-01-30 20:54:51 +0000

    changed js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("nav.html")
+document.addEventListener("DOMContentLoaded", function () {
+    let navPath = window.location.pathname.includes("/pages/") ? "../nav.html" : "nav.html";
+
+    fetch(navPath)
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar").innerHTML = data;
